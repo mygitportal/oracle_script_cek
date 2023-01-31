1b8813c6d73f704e8db7cb9cd7235b6add8cd2fb
mygitportal <122268093+mygitportal@users.noreply.github.com>
2023-01-31 11:10:53 +0700

    Update tbs_usage_more80.sql

--- tbs_usage_more80.sql
+++ tbs_usage_more80.sql
@@ -23,9 +23,9 @@ order 	by ((ddf.bytes-dfs.bytes)/ddf.bytes) desc;
 set lines 500 pages 500
 select ddf.tablespace_name,
 	to_char(ddf.bytes,'999G999G999G999G999')"Aallocated",
-	to_char((ddf.bytes-dfs.bytes),'999G999G999G999G999')"MB Used",
+	to_char((ddf.bytes-dfs.bytes),'999G999G999G999G999')"GB Used",
 	to_char(round(((ddf.bytes-dfs.bytes)/ddf.bytes)*100,2),'990.90')"%Used",
-	to_char(dfs.bytes,'999G999G999G999G999')"MB Free",
+	to_char(dfs.bytes,'999G999G999G999G999')"GB Free",
 	to_char(round((1-((ddf.bytes-dfs.bytes)/ddf.bytes))*100,2),'990.90')"%Free"
 from 
 	(select tablespace_name, (sum(bytes)/1024/1024/1024) bytes from dba_data_files group by tablespace_name) ddf,
